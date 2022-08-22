a3976d0939701edabe424d901683e99c99bc597b
ramchennuru <ramprasad.chennuru@thoughtworks.com>
2022-08-22 14:33:09 +0530

    upadte Factorial class

--- FactorialMain.java
+++ FactorialMain.java
@@ -1,7 +1,7 @@
 public class FactorialMain {
     public static void main(String[] args) {
         Factorial factorial=new Factorial();
-        System.out.println(factorial.val(15));
+        System.out.println(factorial.val(4));
     }
 
 }
